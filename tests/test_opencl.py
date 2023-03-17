2eb50b7964003fe66c379105eb88d78678a25a97
Guillaume Cleroux <guicleroux@icloud.com>
2023-03-16 20:28:39 -0400

    Nvidia support for OpenCL

--- tests/test_opencl.py
+++ tests/test_opencl.py
@@ -1,5 +1,5 @@
 """
-Testing OpenCL support for intel GPU
+Testing OpenCL support for intel/nvidia GPU
 
 Author: Guillaume Cleroux
 """
@@ -8,16 +8,15 @@ import subprocess as sp
 from . import TESTS_PATH
 
 
-def test_intel_clinfo():
+def test_clinfo():
     """Testing supported platform"""
     output = sp.run(["clinfo"], capture_output=True, text=True, check=True)
-    print(output)
 
     assert (
         output.stdout != "Number of platforms                               0\n"
-    ), "No detected platform for intel GPU"
+    ), "No detected platform for linked GPU"
 
 
-def test_intel_opencl():
+def test_opencl():
     """Testing OpenCL support"""
     sp.run(["python3", f"{TESTS_PATH}/opencl/opencl.py"], check=True)
